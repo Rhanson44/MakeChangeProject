730c72ac9e033129138aab8abe17088473c251aa
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-21 17:19:47 -0600

    add getChange()

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,10 +6,16 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter an item  price: ");
+		System.out.print("Enter an item price: ");
 		double price = scan.nextDouble();
 		System.out.print("Enter amount tendered: ");
 		double amount = scan.nextDouble();
+		scan.close();
+	}
+	
+	public static double getChange(double price, double amount) {
+		double change = 0.0;
+		return 0.0;
 	}
 
 }
