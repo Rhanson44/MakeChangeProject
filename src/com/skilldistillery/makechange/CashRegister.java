0ca7acc4dab30abb0b604b5273b74fd31d1b653b
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-21 17:15:06 -0600

    scanner

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,10 +1,15 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter an item  price: ");
+		double price = scan.nextDouble();
+		System.out.print("Enter amount tendered: ");
+		double amount = scan.nextDouble();
 	}
 
 }
