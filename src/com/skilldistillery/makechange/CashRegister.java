6bb52e1c9968c0b90defe1822ff7d3132776c4eb
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-22 09:11:22 -0600

    minimum viable product

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.makechange;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CashRegister {
@@ -7,15 +8,19 @@ public class CashRegister {
 	static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("#.00");
+		
 		System.out.print("Enter an item price: ");
 		double price = scan.nextDouble();
 		System.out.print("Enter amount tendered: ");
 		double amount = scan.nextDouble();
 		
 		double change = getChange(price, amount);
-		System.out.println(change);
+		System.out.println("Your change is: " + df.format(change));
 		
-		makeDenomination(price, amount);
+		makeDenomination(change);
+		
+		System.out.println("Thanks! Have a great day!");
 		scan.close();
 	}
 	
@@ -29,47 +34,65 @@ public class CashRegister {
 			System.out.print("Enter amount tendered: ");
 			amount = scan.nextDouble();
 			}
-		change = amount - price;
+			change = amount - price;
 		}
 		return change;
 	}
 	
-	public static void makeDenomination(double price, double amount) {
-		double change = amount - price;
+	public static void makeDenomination(double change) {
 		
 		int bills = (int) (change);
 		double coins = change - bills;
 		int cents = (int) Math.round((coins * 100));
 		
-		int quarters = cents / 25;
-		cents %= 25;
-		System.out.println(quarters + " quarters");
-		int dimes = cents / 10;
-		cents %= 10;
-		System.out.println(dimes + " dimes");
-		int nickles = cents / 5;
-		cents %= 5;
-		System.out.println(nickles + " nickles");
-		int pennies = cents;
-		System.out.println(pennies + " pennies");
-		
-		
 		int hundreds = (bills / 100);
 		bills %= 100;
-		System.out.println(hundreds + " hundreds");
+		if (hundreds > 0) {
+			System.out.println(hundreds + " hundred dollar bill(s)");
+		}
 		int fifties = (bills / 50); 
 		bills %= 50;
-		System.out.println(fifties + " fifties");
+		if (fifties > 0) {
+			System.out.println(fifties + " fifty dollar bill(s)");
+		}
 		int twenties = (bills / 20); 
 		bills %= 20;
-		System.out.println(twenties + " twenties");
+		if (twenties > 0) {
+			System.out.println(twenties + " twenty dollar bill(s)");
+		}
 		int tens = (bills / 10); 
 		bills %= 10;
-		System.out.println(tens + " tens");
+		if (tens > 0) {
+			System.out.println(tens + " ten dollar bill(s)");
+		}
 		int fives = (bills / 5);
 		bills %= 5;
-		System.out.println(fives + " fives");
+		if (fives > 0) {
+			System.out.println(fives + " five dollar bill(s)");
+		}
 		int ones = bills;
-		System.out.println(ones + " ones");
+		if (ones > 0) {
+			System.out.println(ones + " one dollar bill(s)");
+		}
+		
+		int quarters = cents / 25;
+		cents %= 25;
+		if (quarters > 0) {
+			System.out.println(quarters + " quarter(s)");
+		}
+		int dimes = cents / 10;
+		cents %= 10;
+		if (dimes > 0) {
+			System.out.println(dimes + " dime(s)");
+		}
+		int nickles = cents / 5;
+		cents %= 5;
+		if (nickles > 0) {
+			System.out.println(nickles + " nickle(s)");
+		}
+		int pennies = cents;
+		if (pennies > 0) {
+			System.out.println(pennies + " pennies");
+		}
 	}
 }
