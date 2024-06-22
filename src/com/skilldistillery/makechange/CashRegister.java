9ee76fac42b485c08bd1d9efbea2a757c756beca
Ryan Hanson <hansonrj8322@gmail.com>
2024-06-21 20:55:37 -0600

    complete getChange()

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -4,18 +4,31 @@ import java.util.Scanner;
 
 public class CashRegister {
 
+	static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter an item price: ");
 		double price = scan.nextDouble();
 		System.out.print("Enter amount tendered: ");
 		double amount = scan.nextDouble();
+		
+		double change = getChange(price, amount);
+		System.out.println(change);
 		scan.close();
 	}
 	
 	public static double getChange(double price, double amount) {
 		double change = 0.0;
-		return 0.0;
+		if (price <= amount) {
+			change = amount - price;
+		} else {
+			while(price > amount) {
+			System.err.println("Invalid payment amount, please enter a valid input");
+			System.out.print("Enter amount tendered: ");
+			amount = scan.nextDouble();
+			}
+		change = amount - price;
+		}
+		return change;
 	}
-
 }
